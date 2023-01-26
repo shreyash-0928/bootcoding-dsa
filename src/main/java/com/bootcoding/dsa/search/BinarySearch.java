f46ca16c564375a76f20bdb01f4431f2a49d30da
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-01-26 23:48:35 +0530

    Binary Search Problem

---
+++ src/main/java/com/bootcoding/dsa/search/BinarySearch.java
@@ -0,0 +1,28 @@
+package com.bootcoding.dsa.search;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int a[] = {2,3,4,5,6,7};
+        int x = 6;
+        int index = binarysearch(a, x, 0, a.length-1);
+        System.out.println(x + " is found at index " + index);
+    }
+    private static int binarysearch(int a[], int x, int left, int right){
+
+        int mid = (left + right)/2;
+
+        if(x==a[mid]){
+            return mid;
+        }
+
+        if(left >= right){
+            return -1;
+        }
+
+        if (x > a[mid]){
+            return binarysearch(a, x, mid+1, right);
+        }else{
+            return binarysearch(a, x, left, mid-1);
+        }
+    }
+}
