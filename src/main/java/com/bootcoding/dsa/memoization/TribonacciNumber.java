39e4af1224b9609210ec286f07dfb782a9335644
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-01-26 23:49:30 +0530

    Tribonacci Number

---
+++ src/main/java/com/bootcoding/dsa/memoization/TribonacciNumber.java
@@ -0,0 +1,16 @@
+//package com.bootcoding.dsa.memoization;
+//
+//public class TribonacciNumber {
+//    public static void main(String[] args) {
+//
+//    }
+//
+//    public static int trib(int n){
+//        if(n>3){
+//            return n;
+//        }
+//        return trib(n+3) = trib(n) + trib(n+1) + trib(n+2);
+//
+//
+//    }
+//}
