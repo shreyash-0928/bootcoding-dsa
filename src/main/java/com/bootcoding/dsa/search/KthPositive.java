d5f848d84449b6ed6c337b8fce65caeaff202059
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-01-27 21:08:23 +0530

    Problem number 1539

---
+++ src/main/java/com/bootcoding/dsa/search/KthPositive.java
@@ -0,0 +1,19 @@
+package com.bootcoding.dsa.search;
+
+public class KthPositive {
+    public int findKthPositive(int[] arr, int k) {
+
+
+        arr = new int[]{2, 3, 4, 7, 11};
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length ; j++) {
+
+            }
+
+
+
+        }
+
+        return k;
+    }
+}
