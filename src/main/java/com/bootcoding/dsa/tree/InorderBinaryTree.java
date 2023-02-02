6d26bb2c19764f191adb08636b776a01149d6bb1
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-02-02 23:09:22 +0530

    Inorder Binary Tree

---
+++ src/main/java/com/bootcoding/dsa/tree/InorderBinaryTree.java
@@ -0,0 +1,52 @@
+package com.bootcoding.dsa.tree;
+
+public class InorderBinaryTree {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(5);
+        TreeNode n6 = new TreeNode(6);
+        TreeNode n7 = new TreeNode(7);
+        TreeNode n8 = new TreeNode(8);
+        TreeNode n9 = new TreeNode(9);
+        TreeNode n10 = new TreeNode(10);
+        TreeNode n11 = new TreeNode(11);
+        TreeNode n12 = new TreeNode(12);
+        TreeNode n13 = new TreeNode(13);
+        TreeNode n14 = new TreeNode(14);
+        TreeNode n15 = new TreeNode(15);
+        TreeNode n16 = new TreeNode(16);
+        TreeNode n17 = new TreeNode(17);
+
+        root.left = n2;
+        root.right = n3;
+        n2.left = n4;
+        n2.right = n5;
+        n3.left = n6;
+        n3.right = n7;
+        n4.left = n8;
+        n4.right = n9;
+        n5.left = n10;
+        n5.right = n11;
+        n6.left = n12;
+        n6.right = n13;
+        n7.left = n14;
+        n7.right = n15;
+        n8.left = n16;
+        n8.right = n17;
+
+
+        inorder(root);
+
+    }
+    public static void inorder(TreeNode root){
+        if (root == null){
+            return;
+        }
+        inorder(root.left);
+        System.out.println(root.data);
+        inorder(root.right);
+    }
+}
