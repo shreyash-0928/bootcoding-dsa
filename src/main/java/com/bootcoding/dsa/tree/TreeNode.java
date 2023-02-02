9415a224115bd40b7071dfba08d2ca8ed72b7b4b
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-02-02 23:09:02 +0530

    Tree Node

---
+++ src/main/java/com/bootcoding/dsa/tree/TreeNode.java
@@ -0,0 +1,15 @@
+package com.bootcoding.dsa.tree;
+
+public class TreeNode {
+
+    int data;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int data){
+        this.data = data;
+        this.left= null;
+        this.right= null;
+    }
+
+}
