7fbaf9869112f56451e2d824ff4a6bdd9fb088e7
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-04-06 00:40:38 +0530

    Copy Reversed Array Into New Array

---
+++ src/main/java/com/bootcoding/dsa/Array/CopyInReverse.java
@@ -0,0 +1,23 @@
+package com.bootcoding.dsa.Array;
+
+import java.util.Arrays;
+
+public class CopyInReverse {
+    public static void main(String[] args) {
+
+        int[] originalArr = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] reverseArr = new int[originalArr.length];
+
+        for (int i = originalArr.length - 1; i >= 0 ; i--) {
+            reverseArr[i] = originalArr[i];
+
+        }
+        System.out.println(Arrays.toString(reverseArr));
+
+
+//        for (int i = 0; i < originalArr.length; i++) {
+//            reverseArr[i] = originalArr[originalArr.length - 1 - i];
+//        }
+//        System.out.println(Arrays.toString(reverseArr));
+    }
+}
