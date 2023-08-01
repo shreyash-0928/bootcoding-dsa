e8ff3729c3a92a9d03a77c192bf080ae5e143c1d
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-08-01 11:43:45 +0530

    CharAt Method

---
+++ src/main/java/com/bootcoding/dsa/String/charAtMethod.java
@@ -0,0 +1,11 @@
+package com.bootcoding.dsa.String;
+
+public class charAtMethod {
+    public static void main(String[] args) {
+        String str = "Programming";
+        int len = str.length();
+        char ch = str.charAt(len/2);
+
+        System.out.println(ch);
+    }
+}
