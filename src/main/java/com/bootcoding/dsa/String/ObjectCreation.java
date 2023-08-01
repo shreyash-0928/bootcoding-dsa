f2a5a09faa56085d39c91b4072681e42d5f6bf1d
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-08-01 13:46:23 +0530

    Object Creation

---
+++ src/main/java/com/bootcoding/dsa/String/ObjectCreation.java
@@ -0,0 +1,16 @@
+package com.bootcoding.dsa.String;
+
+public class ObjectCreation {
+    public static void main(String[] args) {
+
+        // There are 2 ways to Create String in JAVA
+
+        // 1st
+        String str = "Dell";
+        System.out.println(str);
+
+        //2nd
+        String str1 = new String("Dell");
+        System.out.println(str1);
+    }
+}
