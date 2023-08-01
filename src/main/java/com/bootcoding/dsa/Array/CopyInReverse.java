d477cc89e4993fb3f818efed9fa20d1bb19bac35
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2023-08-01 13:45:54 +0530

    copy in reverse

--- src/main/java/com/bootcoding/dsa/Array/CopyInReverse.java
+++ src/main/java/com/bootcoding/dsa/Array/CopyInReverse.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class CopyInReverse {
     public static void main(String[] args) {
 
-        int[] originalArr = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] originalArr = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
         int[] reverseArr = new int[originalArr.length];
 
         for (int i = originalArr.length - 1; i >= 0 ; i--) {
